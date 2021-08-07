a10be130a21d5e07f775c061dc3704523359aa73
Thanh Pham <zlatan.pham@gmail.com>
2021-08-07 14:34:15 +0700

    feat: init packages

---
+++ packages/hooks/src/usePrevious.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * React hook that tracks previous value
+ *
+ * @param value the value to track
+ */
+export default function usePrevious<T>(value: T) {
+  const valueRef = useRef<T>();
+
+  useEffect(() => {
+    valueRef.current = value;
+  }, [value]);
+
+  return valueRef.current as T;
+}
