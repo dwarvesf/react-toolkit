8bc9e845b8eeb0864b81ffae776bea6fe2de0bfc
Thanh Pham <zlatan.pham@gmail.com>
2021-08-12 10:38:09 +0700

    feat: add useClipboard

--- packages/hooks/src/index.tsx
+++ packages/hooks/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as useDisclosure } from "./useDisclosure"
 export { default as usePrevious } from "./usePrevious"
 export { default as useForceUpdate } from "./useForceUpdate"
+export { default as useClipboard } from "./useClipboard"
