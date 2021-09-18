e7a17875bbf7fbf580a13a5aff6ff742d3f3a941
Thanh Pham <zlatan.pham@gmail.com>
2021-09-18 10:30:24 +0700

    fix(hooks): remove useFetch and useFetchWithCache

---
+++ .changeset/nervous-countries-mate.md
@@ -0,0 +1,5 @@
+---
+'@dwarvesf/react-hooks': minor
+---
+
+Remove useFetch and useFetchWithCache
--- tsdx.config.base.js
+++ tsdx.config.base.js
@@ -1,15 +1,15 @@
 module.exports = {
   rollup(config, opts) {
     if (opts.format === 'esm') {
-      config = { ...config, preserveModules: true };
+      config = { ...config, preserveModules: true }
       config.output = {
         ...config.output,
         dir: 'dist/',
         entryFileNames: '[name].esm.js',
-      };
-      delete config.output.file;
+      }
+      delete config.output.file
     }
 
-    return config;
+    return config
   },
-};
+}
